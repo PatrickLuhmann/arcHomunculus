927f30448a0ebbc6aac97f45d33f1d0bc7c07ee1
Patrick Luhmann <patrick.luhmann@gmail.com>
2018-03-28 21:59:06 -0700

    Add temp data for splits list
    Added the ItemsSource for the splitsListView. Defined the columns. Used
    three different ways to specify the layout. For the TextBox items that I
    wanted to be centered, I was forced to use either the DataTemplate or the
    Style in the Resources section.
    
    Renamed Class1.cs to SplitsViewModel.cs to be more descriptive.
    
    Signed-off-by: Patrick Luhmann <patrick.luhmann@gmail.com>

---
+++ Homunculus_ViewModel/SplitsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homunculus_ViewModel
+{
+    public class SplitsViewModel
+    {
+		public SplitsViewModel()
+		{
+			// TODO: Placeholder data???
+			splitList = new List<SplitVM>();
+			splitList.Add(new SplitVM { SplitName = "Last Giant", CurrentValue = "0", DiffValue = "0", PbValue = "0 (0)" });
+			splitList.Add(new SplitVM { SplitName = "Pursuer", CurrentValue = "0", DiffValue = "0", PbValue = "1 (1)" });
+		}
+		private List<SplitVM> splitList;
+		public List<SplitVM> SplitList { get { return splitList; } }
+    }
+
+	public class SplitVM
+	{
+		public string SplitName { get; set; }
+		public string CurrentValue { get; set; }
+		public string DiffValue { get; set; }
+		public string PbValue { get; set; }
+	}
+}
