c0b8e08934ac006e99874d1debb84b9c8f91ecc3
Patrick Luhmann <patrick.luhmann@gmail.com>
2018-04-13 16:58:28 -0700

    Split list and success/failure buttons
    The buttons now basically work. The Failure button causes the current value
    to increment. The Success button moves the "cursor" to the next split in the
    list. The Current and Diff fields are updated, but the PB is not; there is
    also no coloring of the splits to indicate the difference. The end of the
    game is also not handled; the cursor simply sits on the last split.
    
    Signed-off-by: Patrick Luhmann <patrick.luhmann@gmail.com>

--- Homunculus_ViewModel/SplitsViewModel.cs
+++ Homunculus_ViewModel/SplitsViewModel.cs
@@ -1,29 +1,117 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Homunculus_ViewModel
 {
     public class SplitsViewModel
-    {
+	{
+		#region View properties
+		private ObservableCollection<SplitVM> splitList;
+		public ObservableCollection<SplitVM> SplitList { get { return splitList; } }
+
+		public string SuccessButtonText { get; set; }
+		public string FailureButtonText {  get { return "Failure"; } }
+		#endregion
+
+		private int CurrentSplit = 0;
+
 		public SplitsViewModel()
 		{
 			// TODO: Placeholder data???
-			splitList = new List<SplitVM>();
-			splitList.Add(new SplitVM { SplitName = "Last Giant", CurrentValue = "0", DiffValue = "0", PbValue = "0 (0)" });
-			splitList.Add(new SplitVM { SplitName = "Pursuer", CurrentValue = "0", DiffValue = "0", PbValue = "1 (1)" });
+			// TODO: Does the Model hold the instantiation as well as the schema???
+			splitList = new ObservableCollection<SplitVM>();
+			splitList.Add(new SplitVM { SplitName = "Last Giant", CurrentValue = 0, DiffValue = 0, CurrentPbValue = 0 });
+			splitList.Add(new SplitVM { SplitName = "Pursuer", CurrentValue = 0, DiffValue = 0, CurrentPbValue = 0 });
+			splitList.Add(new SplitVM { SplitName = "Last Giant", CurrentValue = 0, DiffValue = 0, CurrentPbValue = 0 });
+			splitList.Add(new SplitVM { SplitName = "Pursuer", CurrentValue = 0, DiffValue = 0, CurrentPbValue = 0 });
+			splitList.Add(new SplitVM { SplitName = "Last Giant", CurrentValue = 0, DiffValue = 0, CurrentPbValue = 0 });
+			splitList.Add(new SplitVM { SplitName = "Pursuer", CurrentValue = 0, DiffValue = 0, CurrentPbValue = 0 });
+			
+			SuccessButtonText = "Success";
 		}
-		private List<SplitVM> splitList;
-		public List<SplitVM> SplitList { get { return splitList; } }
-    }
 
-	public class SplitVM
+
+		public void SuccessProc()
+		{
+			// Go to the next split.
+			CurrentSplit++;
+
+			// If we are past the end, then we have won the game.
+			if (CurrentSplit == SplitList.Count)
+			{
+				// TODO: Handle "game won" condition here.
+				CurrentSplit--;
+			}
+		}
+
+		public void FailureProc()
+		{
+			// Increment the current value of the current split.
+			splitList[CurrentSplit].CurrentValue++;
+
+			// Recalculate the difference.
+			splitList[CurrentSplit].DiffValue = 
+				splitList[CurrentSplit].CurrentPbValue - splitList[CurrentSplit].CurrentValue;
+		}
+	}
+
+	public class SplitVM : INotifyPropertyChanged
 	{
 		public string SplitName { get; set; }
-		public string CurrentValue { get; set; }
-		public string DiffValue { get; set; }
-		public string PbValue { get; set; }
+
+		private int currentValue;
+		public int CurrentValue
+		{
+			get
+			{
+				return currentValue;
+			}
+			set
+			{
+				currentValue = value;
+				NotifyPropertyChanged();
+			}
+		}
+
+		private int diffValue;
+		public int DiffValue { get { return diffValue; }
+			set
+			{
+				diffValue = value;
+				NotifyPropertyChanged();
+			}
+		}
+
+		private int currentPbValue;
+		public int CurrentPbValue
+		{
+			get
+			{
+				return currentPbValue;
+			}
+			set
+			{
+				currentPbValue = value;
+				NotifyPropertyChanged();
+			}
+		}
+
+		// I copied this from a web page. I do not know what it does.
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
 	}
 }
--- Homunculus_WPF/MainWindow.xaml.cs
+++ Homunculus_WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Homunculus_ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,29 @@ namespace Homunculus_WPF
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			splitsListView.SelectedIndex = 0;
+		}
+
+		private void failureButton_Click(object sender, RoutedEventArgs e)
+		{
+			// TODO: I don't like this implementation. I don't want the View to 
+			// know the name of the class of the ViewModel because they should be
+			// separate. However, this same class name is given in the XAML in
+			// the DataContext section, so what is the big deal?
+			((SplitsViewModel)DataContext).FailureProc();
+		}
+
+		private void successButton_Click(object sender, RoutedEventArgs e)
+		{
+			// Let the ViewModel do its thing.
+			((SplitsViewModel)DataContext).SuccessProc();
+
+			// Advance the selected row in the ListView.
+			// TODO: This should be fancier, such as with a star at the end.
+			splitsListView.SelectedIndex++;
+			if (splitsListView.SelectedIndex == splitsListView.Items.Count)
+				splitsListView.SelectedIndex = -1;
 		}
 	}
 }
