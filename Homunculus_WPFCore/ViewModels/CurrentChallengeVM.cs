7f64981cba3a1aa507738419fc1602a8dda5d933
Patrick Luhmann <patrick.luhmann@gmail.com>
2019-06-08 13:32:38 -0700

    POS add challenge list and current challenge views (no content yet)
    Signed-off-by: Patrick Luhmann <patrick.luhmann@gmail.com>

---
+++ Homunculus_WPFCore/ViewModels/CurrentChallengeVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Homunculus_WPFCore.ViewModels
+{
+	public class CurrentChallengeVM
+	{
+		public ObservableCollection<SplitVM> Splits { get; set; }
+
+		public CurrentChallengeVM()
+		{
+			Splits = new ObservableCollection<SplitVM>();
+		}
+	}
+}
---
+++ Homunculus_WPFCore/ViewModels/SplitVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homunculus_WPFCore.ViewModels
+{
+	public class SplitVM
+	{
+		public string Name { get; set; }
+		public int CurrentValue { get; set; }
+
+		public SplitVM()
+		{
+			Name = "Default split name";
+			CurrentValue = 17;
+		}
+	}
+}
