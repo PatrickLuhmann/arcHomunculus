93a853971d712fbff7cb279c68ca8150d6f96e1b
Patrick Luhmann <patrick.luhmann@gmail.com>
2019-06-12 15:32:05 -0700

    Change ChallengesView to be more flexible
    I was having difficulty figuring out how to communicate changes between
    MainVM and ChallengesVM. I decided to try allocating a new ChallengesView
    object each time a change to the global app state occurs. Currently, this
    is implemented with two buttons that simulate creating a new data store and
    loading an existing data store. Clicking either of these buttons causes
    MainVM to change the contents of the data store, then a new ChallengesView
    is created, which accesses the new data store in its constructor.
    
    There is probably a better way to do this, but try this way for now.
    
    Signed-off-by: Patrick Luhmann <patrick.luhmann@gmail.com>

--- Homunculus_WPFCore/ViewModels/MainVM.cs
+++ Homunculus_WPFCore/ViewModels/MainVM.cs
@@ -1,8 +1,11 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using Homunculus_ModelCore;
+using Homunculus_WPFCore.Views;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Controls;
 
 namespace Homunculus_WPFCore.ViewModels
 {
@@ -13,12 +16,43 @@ namespace Homunculus_WPFCore.ViewModels
 
 		public string Salutation { get; set; }
 
+		public UserControl ViewForChallenges { get; set; }
+
+		public RelayCommand RelayButtonLoadDataStore { get; private set; }
+		private void LoadDataStore()
+		{
+			TheDynamicDataStore = new DataStoreDynamic();
+
+			// POC - add a random number of challenges to simulate a data store with stuff in it.
+			Random rng = new Random();
+			int qty = rng.Next(10);
+			for (int i = 0; i < qty; i++)
+			{
+				ChallengeCore challenge = new ChallengeCore();
+				challenge.Name = $"Challenge {i}";
+				TheDynamicDataStore.AddChallenge(challenge);
+			}
+
+			ViewForChallenges = new ChallengesView();
+			RaisePropertyChanged("ViewForChallenges");
+		}
+
+		public RelayCommand RelayButtonNewDataStore { get; private set; }
+		private void NewDataStore()
+		{
+			TheDynamicDataStore = new DataStoreDynamic();
+
+			ViewForChallenges = new ChallengesView();
+			RaisePropertyChanged("ViewForChallenges");
+		}
+
 		public MainVM()
 		{
 			TheNameOfTheDataStore = "Homunculuses R Us";
 
-			if (TheDynamicDataStore == null)
-				TheDynamicDataStore = new DataStoreDynamic();
+			// Relay commands
+			RelayButtonLoadDataStore = new RelayCommand(LoadDataStore);
+			RelayButtonNewDataStore = new RelayCommand(NewDataStore);
 
 			Salutation = "Greetings and felicitations to Homunculus [Core]";
 		}
